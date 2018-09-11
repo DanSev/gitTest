95990cea44b84d2b949f0dd75675bb43a3110e95
dse <daniel.severins@gmx.de>
2018-09-11 15:04:15 +0200

    change array size in main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#define SIZE 6
+#define SIZE 8
 
 int main()
 {
-	int bla[] = {1, 3, 4, 5, 7, 3};
+	int bla[] = {1, 3, 4, 5, 7, 3, 2, 4};
 
 	for(int i = 0; i < SIZE; i++)
 	{
