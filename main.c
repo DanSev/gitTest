8fef63483f854c62c60af21423652d68cd0575a9
dse <daniel.severins@gmx.de>
2018-09-11 15:03:01 +0200

    added main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define SIZE 6
+
+int main()
+{
+	int bla[] = {1, 3, 4, 5, 7, 3};
+
+	for(int i = 0; i < SIZE; i++)
+	{
+		printf("%d: %d\n", i, bla[i]);
+	}
+
+	return 0;
+}
+
+
